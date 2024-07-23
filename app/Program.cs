c6bbf5d3700ee6099da95ce6e2dd5be3a67842c8
Jakub Míšek <jmisek@gmail.com>
2024-07-23 13:15:35 +0200

    sample `app`  + vscode `launch.json`
    - WordPress + WooCommerce
    - .NET 8.0
    - localhost:5004
    - requires MySQL on port 3306 with database `wordpress` (configurable, see https://github.com/iolevel/wpdotnet-sdk?tab=readme-ov-file#configuration)

---
+++ app/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Demo application with WordPress.
+ */
+
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5004/")
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddResponseCompression();
+
+            services.AddWordPress(options =>
+            {
+                //
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, IConfiguration configuration)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // add wordpress into the pipeline
+            // using default configuration from appsettings.json (IConfiguration), section WordPress
+            // using empty set of .NET plugins
+            app.UseWordPress();
+
+            app.UseDefaultFiles();
+        }
+    }
+}
